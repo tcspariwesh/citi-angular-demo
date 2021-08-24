f7f8e383730d58e71efc9b5c45406f8d0651274d
tcspariwesh <tcspariwesh@gmail.com>
2021-08-24 17:12:16 +0530

    pass data from parent to child component

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent implements OnInit {
-
+  @Input()
+  fromParent='';
   constructor() { }
 
   ngOnInit(): void {
