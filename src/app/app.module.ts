309600a3bd116c9d4bc0e4587925f83199764564
tcspariwesh <tcspariwesh@gmail.com>
2021-08-24 17:04:29 +0530

    added new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
 import { HttpClientModule } from '@angular/common/http';
+import { CounterComponent } from './counter/counter.component';
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [//all components 
+    AppComponent, CounterComponent
   ],
   imports: [
     BrowserModule, FormsModule,  HttpClientModule
